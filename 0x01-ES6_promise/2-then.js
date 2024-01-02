7338500b35d2ee6ee9ea940524aab61852ae0359
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-02 18:36:32 +0100

    refactor: Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
+    console.log('Got a response from the API'); // Log for every resolution
     promise.then((response) => {
-      console.log('Got a response from the API'); // Log for every resolution
       return { status: 200, body: 'success' }; // Return desired object on resolve
     })
     .catch((error) => {
-      console.log('Got a response from the API'); // Log for every resolution
       return new Error(); // Return empty error object on reject
     });
 }
