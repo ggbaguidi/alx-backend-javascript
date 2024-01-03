864100db8de2565c9b1e2c208b631c3bb8855bae
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-03 14:27:17 +0100

    fix: rewrite the script v4

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise
-    .allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((value) => {
       value.map((o) => ({
         status: o.status,
