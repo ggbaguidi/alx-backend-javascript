82c0900946ff0f2a106a4afb05a1f951ec16f0d3
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-03 14:01:53 +0100

    feat: add Handle multiple promises feature

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((value) => {
+      value.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }));
+    });
+}
