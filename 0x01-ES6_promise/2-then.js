b88a70d2453329ccfb43ccf0f211e856475c8023
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-03 13:47:05 +0100

    style: reformat the code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,8 @@
 function handleResponseFromAPI(promise) {
-    console.log('Got a response from the API'); // Log for every resolution
-    promise.then((response) => {
-      return { status: 200, body: 'success' }; // Return desired object on resolve
-    })
-    .catch((error) => {
-      return new Error(); // Return empty error object on reject
-    });
+  console.log('Got a response from the API'); // Log for every resolution
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error());
 }
 
 export default handleResponseFromAPI;
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    return  Promise
-            .all([uploadPhoto(), createUser()])
-            .then((response) => {
-                console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
-            })
-            .catch((error) => console.log("Signup system offline"));
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((response) => {
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
