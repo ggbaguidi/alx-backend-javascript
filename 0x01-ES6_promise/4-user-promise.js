ef156fcf87bb61716af0b9c7ae0b82e0490102d4
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-03 10:52:10 +0100

    fix: remove the node env path to the file

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,8 @@
 function signUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-        resolve({firstName, lastName});
-    });
+    return Promise.resolve({
+        firstName: firstName,
+        lastName: lastName
+      });
 }
 
 export default signUpUser;
