bd1d3de86f82e82f2afbec8e7ffe0d1d628ad242
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-03 13:33:05 +0100

    fix: change the variable type 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,16 @@
 export default function taskBlock(trueOrFalse) {
-  let task;
-  let task2;
+  // eslint-disable-next-line no-use-before-define, block-scoped-var
+  task = false;
+  // eslint-disable-next-line no-use-before-define, block-scoped-var
+  task2 = true;
 
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    // eslint-disable-next-line vars-on-top, no-var
+    var task = false;
+    // eslint-disable-next-line vars-on-top, no-var
+    var task2 = true;
   }
 
+  // eslint-disable-next-line block-scoped-var
   return [task, task2];
 }
