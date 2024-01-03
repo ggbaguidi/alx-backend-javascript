88cf40c9c08409c88f51763b3d9b27c9ce4f81f7
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-01-03 13:51:02 +0100

    fix: error Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,12 @@
 function handleResponseFromAPI(promise) {
   console.log('Got a response from the API'); // Log for every resolution
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error());
+  if (promise) {
+    return {
+      status: 200,
+      body: 'Success',
+    };
+  }
+  return (new Error());
 }
 
 export default handleResponseFromAPI;
